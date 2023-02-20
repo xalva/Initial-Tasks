0922ac6e05c99f5566075a52f1dbf751aa3f60ac
Nekliudov <nekliudov88@gmail.com>
2023-02-20 20:25:30 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+var users []User
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	content, err := ioutil.ReadFile("users.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(content, &users)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/books", getBooks).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
